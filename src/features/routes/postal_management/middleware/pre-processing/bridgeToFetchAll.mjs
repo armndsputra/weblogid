8f46be18b680aacb6949dd8d5bd78459ebc8bbeb
trisna7991 <trisna.tkjtamsisjetis14@gmail.com>
2025-11-10 12:23:07 +0700

    repair pagination

--- src/features/routes/postal_management/middleware/pre-processing/bridgeToFetchAll.mjs
+++ src/features/routes/postal_management/middleware/pre-processing/bridgeToFetchAll.mjs
@@ -1,26 +1,37 @@
+// pagination
+async function _page_breaker( limit, offset) {
 
-// ?limit=5&offset=0
+    if (isNaN(limit) || isNaN(offset)) {
+        throw new Error('NOT_NUMBER');
+    }
+
+    if (limit < 0 || offset < 0) {
+            throw new Error('NEGATIVE_VALUES_NOT_ALLOWED')
+    }
+
+    return { limit, offset }
+}
+
+// http://localhost:3000/postal/?limit=2&offset=0 example pagination
 export const bridgeFetchAll = async ( req, res, next ) => {
+
+    const { limit, offset } = req.query
 
     try {
 
-        // 1. fetch query value
-        const { limit, offset } = req.query
-
-        if (isNaN(limit) || isNaN(offset)) {
-            throw new Error('NOT_NUMBER');
-        }
+        // 1. cek value query
+        const result = await _page_breaker( limit, offset )
         
-        return
-        req.data = { limit , offset }
+        // 2. data has been verified
+        req.data = result
         next()
 
     } catch (err) {
         // handle error
         console.error(err)
-        if (err.message === 'NOT_NUMBER') {
+        if (err.message === 'NOT_NUMBER' || 'NEGATIVE_VALUES_NOT_ALLOWED') {
             return res.status(400).json({
-                error: 'limit & offset have to number'
+                error: 'limit & offset have to number and positive number!'
             });
         }
         
